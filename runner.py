aa89f7e51857320756be4cf8563d1aaeecb43529
fcossi1 <franco_carbajal@live.com>
2024-10-27 18:12:36 -0400

    Implemented Load CSV function. Updated README.

--- runner.py
+++ runner.py
@@ -1,12 +1,13 @@
+import pandas as pd
 
-def loadFiles(oaFile, pcFile, csv, pcServFile):
+def loadFiles(oaFile: str, pcFile: str, csv: pd.DataFrame, pcServFile):
     oaAPI = loadAPIFile(oaFile)
     pcAPI = loadAPIFile(pcFile)
-    csvData = ''
+    csvData = loadCSV(csv)
     pcServer = loadAPIFile(pcServFile)
     return oaAPI, pcAPI, csvData, pcServer
 
-def loadAPIFile(fileName):
+def loadAPIFile(fileName: str):
     api = ''
     try:
         with open(fileName, 'r') as file:
@@ -16,14 +17,33 @@ def loadAPIFile(fileName):
         return ''
     return api
 
+def loadCSV(fileName: str):
+    data = pd.read_csv(fileName)
+    parsedData = []
+    for i, row in data.iterrows():
+        rowData = {
+            'id': row['id'],
+            'type': row['type'],
+            'title': row['title'],
+            'content': row['content'],
+            'envId': row['envId'],
+            'refId': row['refId']
+        }
+        parsedData.append(rowData)
+    return parsedData
+
 def main():
     print('Start project straightforward')
     print('Load files')
     oaFile = 'inputFiles/APIKey.txt'
     pcFile = 'inputFiles/pcAPIKey.txt'
-    csv = 'inputFiles/testVectors-csv-2024-10-14.txt'
+    csv = 'inputFiles/testVectors-csv-2024-10-14.csv'
     pcServFile = 'inputFiles/pcServer.txt'
     oaAPI, pcAPI, csvData, pcServ = loadFiles(oaFile, pcFile, csv, pcServFile)
-    print('OpenAI API:', oaAPI, pcAPI, pcServ)
+    print('OpenAI API:', oaAPI)
+    print('Pinecone API:', pcAPI)
+    print('CSV:')
+    print([row['id'] for row in csvData])
+    print('Pinecone Server:', pcServ)
 
 main()
