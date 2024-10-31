2d3d734afe28a1f8ed587239be9ad12b657a828b
fcossi1 <franco_carbajal@live.com>
2024-10-31 17:06:12 -0400

    Implemented an error counter for add vectors.

--- runner.py
+++ runner.py
@@ -62,8 +62,9 @@ def ps_defineIndex(pc: pinecone.Pinecone, indexName: str):
 def ps_addVectors(oa: openai.OpenAI, csvData: list, embModel: str):
     vectors = []
     #print(csvData)
+    countErrors = 0
     for entry in csvData:
-        print('a', entry)
+        #print('a', entry)
         try:
             vector = {
                 'id': str(entry['id']),
@@ -72,23 +73,32 @@ def ps_addVectors(oa: openai.OpenAI, csvData: list, embModel: str):
                     'title': entry['title']
                 }
             }
+            if len(vector['values']) == 0:
+                countErrors += 1
+                print('Due to error, embedding will not be added as vector.')
+                print('Error count:', countErrors)
+                continue
             vectors.append(vector)
+            countErrors = 0
         except AttributeError as e:
             print('Could not complete vector assignment:', e)
-            return []
+            countErrors += 1
+            print('Error count:', countErrors)
+        if countErrors == 3:
+            print('Error limit reached. Breaking operation.')
+            break
+            
     return vectors
 
 def ps_av_generateEmbedding(oa: openai.OpenAI, content: str, embModel: str):
     try:
         res = oa.embeddings.create(input=content, model=embModel)
-        print('ad')
-        print('res', res.data[0].embedding)
         embedding = res.data[0].embedding
         print(f"Generated embedding of length {len(embedding)}")
         return embedding
     except Exception as e:
         print(f"Error generating embedding: {e}")
-    return None
+    return []
 
 def ps_uploadVectors(index: pinecone.Index, vectors: list, batchSize: int):
     vectorsLength = len(vectors)
@@ -117,10 +127,10 @@ def main():
     print('Start project straightforward')
     print('Load files')
     oaFile = 'inputFiles/APIKey.txt'
-    pcFile = 'inputFiles/pcAPIKey.txt'
-    csv = 'inputFiles/testVectors-csv-2024-10-14.csv'
+    pcFile = 'inputFiles/pcAPIKey2.txt'
+    csv = 'inputFiles/vectors-csv-2024-10-14.csv'
     oaAPI, pcAPI, csvData = loadFiles(oaFile, pcFile, csv)
-    indexName = 'indextestsf'
+    indexName = 'indextestsf2'
     embModel = 'text-embedding-3-large'
     batchSize = 2
     pineconeService(oaAPI=oaAPI, pcAPI=pcAPI, csvData=csvData, indexName=indexName, embModel=embModel, batchSize=batchSize)
