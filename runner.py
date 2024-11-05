e63cd9bf734cbf3aeb548291b273bcd20a122528
fcossi1 <franco_carbajal@live.com>
2024-11-05 17:15:03 -0500

    Implementation of user welcome function in runner file.

--- runner.py
+++ runner.py
@@ -1,3 +1,18 @@
+def userWelcome():
+    print('Welcome to the Pinecone Embedding and Vector Uploader!')
+    print('Press 1 to start a new upload')
+    print('Press 2 to continue an upload from saved file')
+    res = input('Your response: ')
+    if res == 1:
+        print('You selected (1) to start a new upload job')
+        userInputNew()
+    elif res == 2:
+        print('You selected (2) to load an existing save file and continue upload job')
+        userInputLoad()
+    else:
+        print('Your selection was INVALID. Please reattempt.')
+        userWelcome()
+
 def main():
     print('Start project OOP')
 
