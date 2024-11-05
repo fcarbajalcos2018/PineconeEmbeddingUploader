a5796c117841c7e04765bd318b222e45d2ffe0d3
fcossi1 <franco_carbajal@live.com>
2024-11-05 17:59:31 -0500

    Implemented Define Index function to Pinecone Service class.

--- models/pineconeService.py
+++ models/pineconeService.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-from pinecone import Pinecone
+from pinecone import pinecone, Pinecone
 
 class PineconeService:
 
@@ -8,3 +8,17 @@ class PineconeService:
         self.pc = Pinecone(api_key=pcAPI)
         self.index = None
         self.vectors = None
+
+    def defineIndex(self, indexName: str):
+        indexes = [index['name'] for index in self.pc.list_indexes()]
+        if len(indexes) == 0 or indexName not in indexes:
+            self.pc.create_index(
+                name=indexName,
+                dimension=3072,
+                metric='cosine',
+                spec=pinecone.ServerlessSpec(
+                    cloud='aws',
+                    region='us-east-1'
+                )
+            )
+        self.index = self.pc.Index(name=indexName)
