d7b8a5753b308476a8467324fd82b21e69780400
fcossi1 <franco_carbajal@live.com>
2024-11-06 16:39:09 -0500

    Implemented remaining code in runner for the vector creation and upload.

--- runner.py
+++ runner.py
@@ -22,6 +22,31 @@ def userWelcome():
     print('Initializing Pinecone service....')
     pc = PineconeService(oaAPI=oaAPI, pcAPI=pcAPI)
     embeddings = _userDefineIndexAndEmbeddings(pinecone=pc, embeddings=embeddingSet)
+    if not isinstance(embeddings, list) or embeddings == None:
+        print('INVALID value for embeddings. Terminating operation.')
+        return
+    vectors = []
+    while True:
+        print('By the number, please select the embedding model as follows:')
+        print('1. text-embedding-3-large')
+        embModel = int(input('Embedding Model: '))
+        if embModel == 1:
+            print('Proceeding to add vectors...')
+            vectors = pc.addVectors(csvData=embeddings, embModel='text-embedding-3-large')
+            break
+        else:
+            print('INVALID field entry. Please reattempt.')
+    print('Vector generation complete.')
+    while True:
+        print('Specify a number for the batch size.')
+        batchSize = int(input('Batch Size: '))
+        if batchSize > 1:         
+            print('Proceeding to upload...')
+            pc.uploadVectors(vectors=vectors)
+            pc.saveFailedEmbeddings()
+            break
+        else:
+            print('INVALID entry. Please reattempt.')
 
 def __insertOaPc(inFile: InputFiles):
     print('Please enter the name of the file containing the Open AI API Key')
@@ -79,13 +104,12 @@ def _userDefineIndexAndEmbeddings(pinecone: PineconeService, embeddings: Any | d
             pinecone.defineIndex(indexName)
         except AttributeError as ae:
             print('INVALID JSON format:', ae)
-        except TypeError as te:
-            print('Embedding list is NOT a list:', te)
     else:
         raise Exception('The file contents could not be read')
     return resEmb
 
 def main():
     print('Start project OOP')
+    userWelcome()
 
 main()
